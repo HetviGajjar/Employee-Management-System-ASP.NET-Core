892c002613ba8306051f27d61f496f5b5975e865
HetviGajjar <119831671+HetviGajjar@users.noreply.github.com>
2026-07-13 11:56:38 +0530

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace crudDemo.Models
+{
+    public class Employee
+    {
+        public required string Name { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+        public required string Department { get; set; }
+        public Char Gender { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace crudDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
